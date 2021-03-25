29f04ca79e882c2f7f2c8aa2eeef504f91c5f67e
olived <oli.dufour@outlook.com>
2021-03-25 23:01:49 +0100

    Update repo

---
+++ docker-image/prestart.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env sh
+
+# Run migrations
+alembic upgrade head
+
--- docker-image/start-reload.sh
+++ docker-image/start-reload.sh
@@ -1,12 +1,7 @@
 #! /usr/bin/env sh
 set -e
 
-if [ -f /app/app/main.py ]; then
-    DEFAULT_MODULE_NAME=app.main
-elif [ -f /app/main.py ]; then
-    DEFAULT_MODULE_NAME=main
-fi
-MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
+MODULE_NAME=${MODULE_NAME:-server.main}
 VARIABLE_NAME=${VARIABLE_NAME:-app}
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
 
